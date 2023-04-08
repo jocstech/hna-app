bed582c2ee4a6000b721916a8ee0fc89a697ead6
Yulong Fang <aionv@qq.com>
2023-04-08 23:16:40 +0800

    refactor: 修正更具路由配置来自动生成页面
    undefined

---
+++ src/pages/itinerary/itinerary.vue
@@ -0,0 +1,7 @@
+<template>
+  <view>行程</view>
+</template>
+
+<script setup lang="ts" name="itinerary"></script>
+
+<style scoped></style>
---
+++ src/pages/services/services.vue
@@ -0,0 +1,7 @@
+<template>
+  <view>服务大厅</view>
+</template>
+
+<script setup lang="ts" name="services"></script>
+
+<style scoped></style>
